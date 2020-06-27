4844db50955b9dd80f8164ddf1ee597f8fb5c369
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-06-26 23:02:10 -0500

    Working Rope Kinda shit

---
+++ Assets/BalloonBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonBehavior : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(0, 1f, 0, ForceMode.Impulse);
+    }
+}
