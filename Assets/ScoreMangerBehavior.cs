a6646cdf75b379b3ba9abb297ab2540057d5d3c2
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-06-28 14:09:03 -0500

    ScoreManager

---
+++ Assets/ScoreMangerBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreMangerBehavior : MonoBehaviour
+{
+    public Text scoreValue;
+    public GameObject cake;
+    private CakeCollectingBehavior cakeCollectingBehavior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreValue.text = "Score: " + cakeCollectingBehavior.Score.ToString();
+    }
+}
